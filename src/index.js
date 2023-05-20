088ba5aedcd92edfbeb38dcd0783377fad03e935
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2023-05-19 22:05:52 -0300

    adcionando execute e writeback para soma

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const instructions = {
 
 let uiCallbacks = {
     issue: () => { },
+    writeBack: () => { },
 }
 
 export function setInstructions(commands, callbacks) {
@@ -27,6 +28,8 @@ export function setInstructions(commands, callbacks) {
 }
 
 export function step() {
+    writeBack();
+    execute()
     issue();
 }
 
@@ -68,17 +71,6 @@ const operations = {
 const stations = {
     1: [
         {
-            id: 0,
-            busy: 0,
-            op: 0,
-            opName: "",
-            vj: 0,
-            vk: 0,
-            qj: 0,
-            qk: 0,
-            cicles: 0,
-        },
-        {
             id: 1,
             busy: 0,
             op: 0,
@@ -100,6 +92,17 @@ const stations = {
             qk: 0,
             cicles: 0,
         },
+        {
+            id: 3,
+            busy: 0,
+            op: 0,
+            opName: "",
+            vj: 0,
+            vk: 0,
+            qj: 0,
+            qk: 0,
+            cicles: 0,
+        },
     ],
 }
 
@@ -110,7 +113,11 @@ function issue() {
     const opcode = instruction & 0b1111111; // [6-0]
     const funct = (instruction >> 12) & 0b11 // [14-12]
 
-    const operation = operations[opcode][funct];
+    const operation = !!operations[opcode] ? operations[opcode][funct] : 0;
+    if (!operation) {
+        console.warn('operation not found')
+        return;
+    }
     const params = operations[opcode].getParams(instruction);
     const station = stations[operation.station].find(station => !station.busy);
 
@@ -136,9 +143,70 @@ function issue() {
     station.busy = true;
     station.op = operation.op;
     station.opName = operation.name;
+    console.log("oi")
     regStats[rd] = station.id;
 
 
-    // pc += 4;
+    pc += 4;
     uiCallbacks.issue(station);
+}
+
+const adder = {
+    busy: false,
+    ready: true,
+    station: null,
+    result: 0
+}
+
+function execute() {
+    if (!adder.busy) {
+        const adderStation = stations[1].find(station => !station.busy && station.qj == 0 && station.qk == 0);
+        if (adderStation) {
+            adder.busy = true;
+            adder.ready = 0;
+            adder.station = adderStation;
+        }
+    } else if (adder.station.cicles == 0) {
+        adder.result = adder.station.op(adder.station.vj, adder.station.vk);
+        adder.ready = 1;
+    } else {
+        adder.cicles--;
+    }
+
+
+}
+
+function writeBack() {
+    const uiStations = new Set();
+    const uiRegisters = new Set();
+    console.log("wb")
+
+    if (adder.ready && adder.busy) {
+        regStats.forEach((stat, i) => {
+            if (stat == adder.station.id) {
+                registers[i] = adder.result;
+                regStats[i] = 0;
+                uiRegisters.add({ id: i, value: adder.result });
+            }
+        })
+
+        stations[1].forEach(station => {
+            if (station.qj == adder.station.id) {
+                station.qj = 0;
+                station.vj = adder.result;
+                uiStations.add(station);;
+            }
+            if (station.qk == adder.station.id) {
+                station.qk = 0;
+                station.vk = adder.result;
+                uiStations.add(station);
+            }
+        });
+
+        adder.busy = false;
+        adder.station.busy = false;
+        uiStations.add(adder.station);
+    }
+
+    uiCallbacks.writeBack(uiStations, uiRegisters);
 }
